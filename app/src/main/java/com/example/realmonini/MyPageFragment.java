b27ee521f852273f708f38ddfb6eadafcd2832cb
88guri <quffl2002@naver.com>
2026-05-05 18:56:10 +0900

    feature#9 마이페이지 뷰 추가, background color로 변경

--- app/src/main/java/com/example/realmonini/MyPageFragment.java
+++ app/src/main/java/com/example/realmonini/MyPageFragment.java
@@ -9,27 +9,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class HomeFragment extends Fragment {
+public class MyPageFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_mypage, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.btn_journal).setOnClickListener(v -> {
-            //저널 작성 화면으로 이동
+        view.findViewById(R.id.btn_logout).setOnClickListener(v -> {
+            // 나중에 로그아웃 처리
         });
 
-        view.findViewById(R.id.ic_mypage).setOnClickListener(v -> {
-            requireActivity().getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.main_frame, new MyPageFragment())
-                    .commit();
+        view.findViewById(R.id.tv_withdraw).setOnClickListener(v -> {
+            // 나중에 회원탈퇴 처리
         });
     }
 }
