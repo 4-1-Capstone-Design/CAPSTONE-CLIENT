ce2b3f308162b3b7ce1a87d7c1655d09d3714e98
88guri <quffl2002@naver.com>
2026-05-18 16:21:24 +0900

    feature#11 저널 작성 완료 페이지

---
+++ app/src/main/java/com/example/realmonini/JournalCompleteFragment.java
@@ -0,0 +1,32 @@
+package com.example.realmonini;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class JournalCompleteFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_journal_complete, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        new Handler().postDelayed(() -> {
+            requireActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_frame, new JournalReplyFragment())
+                    .commit();
+        }, 2000);
+    }
+}
