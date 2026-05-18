e01d92d87709e53ae4108688c7762d4ce4c0b851
88guri <quffl2002@naver.com>
2026-05-18 16:43:57 +0900

    feature#13 특정 일의 저널 다시보기 뷰 구현

---
+++ app/src/main/java/com/example/realmonini/JournalDetailFragment.java
@@ -0,0 +1,31 @@
+package com.example.realmonini;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class JournalDetailFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_journal_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.btn_check_reply).setOnClickListener(v -> {
+            requireActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_frame, new JournalReplyFragment())
+                    .commit();
+        });
+    }
+}
